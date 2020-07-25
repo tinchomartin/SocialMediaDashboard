ab68099bc0fd31d743cb875ab076023dd524b338
Martin <mgaraypagano@gmail.com>
2020-07-25 18:28:21 -0300

    actualizado el toggler, el modo nocturno y el dash secundario

--- darkMode.js
+++ darkMode.js
@@ -1,5 +1,14 @@
-const check = document.getElementById("changer");
-  check.addEventListener('change',e => {
-    e.preventDefault;
-   document.body.classList.toggle("dark-theme");
-  })
+// const check = document.getElementById("changer");
+//   check.addEventListener('change',e => {
+//     e.preventDefault;
+//    document.body.classList.toggle("dark-theme");
+//   })
+
+const toggleClick = document.querySelector(".toggler");
+slider = document.querySelector(".slider");
+
+function darkMode() {
+  toggleClick.classList.toggle("dark-theme");
+  slider.classList.toggle("dark-theme");
+  document.body.classList.toggle("dark-theme");
+}
